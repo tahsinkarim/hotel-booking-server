479d5532515a600621ebf4a1c219fbdb40d0de48
Tahsin Karim <tahsinkarim.dev@gmail.com>
2023-01-04 18:52:36 +0600

    updated api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 const connect = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
