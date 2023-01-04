7adfc214322814f93ba4dd11dd208ee692af973f
Tahsin Karim <tahsinkarim.dev@gmail.com>
2023-01-04 18:39:13 +0600

    updated api

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ const port = process.env.PORT || 8800;
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
-    console.error(err);
     throw err;
   }
 };
